99b6b2211b61ecb87c28119e36ddf25f35f2ee1f
alexiswu <wuxinyi2000@gmail.com>
2022-10-19 00:58:11 -0400

    updated version of n bit adders

--- labs/01_game_of_life/hdl/adder_n.sv
+++ labs/01_game_of_life/hdl/adder_n.sv
@@ -1,11 +1,8 @@
 `timescale 1ns/1ps
 `default_nettype none
+// `include "cla_adder_1.sv"
 
-// You may find it useful to have a way to build an n-bit adder, 
-// or you can manually create them. 
 
-// This module shows how to use a generate statement to connect
-// n 1-bit adders with carries to make a ripple carry adder.
 module adder_n(a, b, c_in, sum, c_out);
 
 parameter N = 32;
@@ -13,24 +10,38 @@ parameter N = 32;
 input  wire [N-1:0] a, b;
 input wire c_in;
 output logic [N-1:0] sum;
-output wire c_out;
+output logic c_out;
 
-wire [N:0] carries;
-assign carries[0] = c_in;
-assign c_out = carries[N];
+
+wire [N-1:0] G, P;
+wire [N:0] C;
+
+assign C[0] = c_in;
+
 generate
-  genvar i;
-  for(i = 0; i < N; i++) begin : ripple_carry
-    adder_1 ADDER (
-      .a(a[i]),
-      .b(b[i]),
-      .c_in(carries[i]),
-      .sum(sum[i]),
-      .c_out(carries[i+1])
-    );
-  end
+    genvar i;
+    for (i=0;i<N;i++) begin
+        adder_1 full_adder_inst(
+            .a(a[i]),
+            .b(b[i]),
+            .c_in(C[i]),
+            .sum(sum[i]),
+            .c_out()
+        );
+    end
 endgenerate
 
+generate
+    genvar j;
+    for (j=0;j<N;j++) begin
+        assign G[j] = a[j] & b[j];
+        assign P[j] = a[j] | b[j];
+        assign C[j+1] = G[j] | (P[j] & C[j]);
+    end
+
+endgenerate
+
+assign c_out = C[N];
+
+
 endmodule
-// to instantiate
-// adder_n #(.N(32)) adder_32bit_a ( port list );
