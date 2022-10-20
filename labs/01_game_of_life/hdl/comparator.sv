9de6972bf4d4efff0c790ec3cdf88dc91fb78514
alexiswu <wuxinyi2000@gmail.com>
2022-10-19 20:35:01 -0400

    update conway_cell

--- labs/01_game_of_life/hdl/comparator.sv
+++ labs/01_game_of_life/hdl/comparator.sv
@@ -1,16 +1,17 @@
 `default_nettype none
 `timescale 1ns/1ps
 
-module comparator (a,b,out);
-// if a>=b: c_out = 0
-// if a<b: c_out = 1 (b/c overflow)
+module comparator (a,b,equal,less,great);
 
-parameter N = 32;
+parameter N = 4;
 
 input wire [N-1:0] a,b
-output wire out;
+output wire equal;
+output wire less;
+output wire great;
 
 wire [N-1:0] subtracter;
+wire [N-1:0] sum;
 assign subtracter = ~ b;
 
 adder_n COMPARE
@@ -18,9 +19,12 @@ adder_n COMPARE
     .a(a),
     .b(subtracter),
     .c_in(1'b1),
-    .sum(),
-    .c_out(out)
+    .sum(sum),  
+    .c_out()
 );
 
+assign equal = ~sum;
+assign less = sum[N-1];
+assign great = ~less;
 
 endmodule
--- labs/01_game_of_life/hdl/conway_cell.sv
+++ labs/01_game_of_life/hdl/conway_cell.sv
@@ -15,8 +15,11 @@ input wire [7:0] neighbors;
 wire c_in;
 wire [3:0] a,b;
 wire [7:0] sum;
+
 wire c_out;
 wire alive_neighbors;
+
+logic next_state;
 
 assign a = neighbors[3:0]
 assign b = neighbors[7:4]
@@ -33,16 +36,32 @@ adder_n ADDER(
 
 assign alive_neighbors = {c_out,sum};
 
-wire less_than;
+wire equal_3;
+wire greater_than_3;
 
-comparator COMPARE(
-    .a()
-)
+comparator COMPARE_3(
+    .a(alive_neighbors),
+    .b(3),
+    .equal(equal_3),
+    .less(),
+    .great(greater_than_3)
+);
 
-always_comb begin : compare
-    if ()
-    
-end
+wire less_than_2;
 
+comparator COMPARE_2(
+    .a(alive_neighbors),
+    .b(2),
+    .equal(),
+    .less(less_than_2),
+    .great()
+);
+
+assign next_state = equal_3 | (state_0  & ~less_than_2) | (state_0 & ~ greater_than_3)
+assign state_d = ~rst ? (ena ? next_state : state_q) : state_0; // enable & rst
+
+  always_ff @(posedge clk ) begin
+    state_q <= state_d;
+  end
 
 endmodule
