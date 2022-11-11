624290b74ac84cf12dd30bf93524333a11c43454
Alexis_Xinyi_Wu <30337200+AlexisWu-01@users.noreply.github.com>
2022-11-11 01:07:00 -0500

    completed register

--- hws/6/hdl/register_file.sv
+++ hws/6/hdl/register_file.sv
@@ -35,7 +35,6 @@ always_comb write_index = wr_addr?(32'b1 << wr_addr) : 32'b0;
 
 
 // write data
-
 register  #(.N(32)) reg_wr1(.clk(clk), .ena(wr_ena & write_index[1]), .rst(1'b0), .d(wr_data), .q(x01));
 register  #(.N(32)) reg_wr2(.clk(clk), .ena(wr_ena & write_index[2]), .rst(1'b0), .d(wr_data), .q(x02));
 register  #(.N(32)) reg_wr3(.clk(clk), .ena(wr_ena & write_index[3]), .rst(1'b0), .d(wr_data), .q(x03));
@@ -69,12 +68,78 @@ register  #(.N(32)) reg_wr30(.clk(clk), .ena(wr_ena & write_index[30]), .rst(1'b
 register  #(.N(32)) reg_wr31(.clk(clk), .ena(wr_ena & write_index[31]), .rst(1'b0), .d(wr_data), .q(x31));
 
 logic [31:0] read_0_index, read_1_index;
-logic [31:0] out_data_0, out_data_1;
+wire [31:0] out_data_0, out_data_1;
 
 // write file decoder
 always_comb read_0_index = rd_addr0?(32'b1 << rd_addr0) : 32'b0;
 always_comb read_1_index = rd_addr1?(32'b1 << rd_addr1) : 32'b0;
 
+register #(.N(32)) reg_rd0_0(.clk(clk), .ena(read_0_index[0]), .rst(1'b0), .d(x00), .q(out_data_0));
+register #(.N(32)) reg_rd0_1(.clk(clk), .ena(read_0_index[1]), .rst(1'b0), .d(x01), .q(out_data_0));
+register #(.N(32)) reg_rd0_2(.clk(clk), .ena(read_0_index[2]), .rst(1'b0), .d(x02), .q(out_data_0));
+register #(.N(32)) reg_rd0_3(.clk(clk), .ena(read_0_index[3]), .rst(1'b0), .d(x03), .q(out_data_0));
+register #(.N(32)) reg_rd0_4(.clk(clk), .ena(read_0_index[4]), .rst(1'b0), .d(x04), .q(out_data_0));
+register #(.N(32)) reg_rd0_5(.clk(clk), .ena(read_0_index[5]), .rst(1'b0), .d(x05), .q(out_data_0));
+register #(.N(32)) reg_rd0_6(.clk(clk), .ena(read_0_index[6]), .rst(1'b0), .d(x06), .q(out_data_0));
+register #(.N(32)) reg_rd0_7(.clk(clk), .ena(read_0_index[7]), .rst(1'b0), .d(x07), .q(out_data_0));
+register #(.N(32)) reg_rd0_8(.clk(clk), .ena(read_0_index[8]), .rst(1'b0), .d(x08), .q(out_data_0));
+register #(.N(32)) reg_rd0_9(.clk(clk), .ena(read_0_index[9]), .rst(1'b0), .d(x09), .q(out_data_0));
+register #(.N(32)) reg_rd0_10(.clk(clk), .ena(read_0_index[10]), .rst(1'b0), .d(x10), .q(out_data_0));
+register #(.N(32)) reg_rd0_11(.clk(clk), .ena(read_0_index[11]), .rst(1'b0), .d(x11), .q(out_data_0));
+register #(.N(32)) reg_rd0_12(.clk(clk), .ena(read_0_index[12]), .rst(1'b0), .d(x12), .q(out_data_0));
+register #(.N(32)) reg_rd0_13(.clk(clk), .ena(read_0_index[13]), .rst(1'b0), .d(x13), .q(out_data_0));
+register #(.N(32)) reg_rd0_14(.clk(clk), .ena(read_0_index[14]), .rst(1'b0), .d(x14), .q(out_data_0));
+register #(.N(32)) reg_rd0_15(.clk(clk), .ena(read_0_index[15]), .rst(1'b0), .d(x15), .q(out_data_0));
+register #(.N(32)) reg_rd0_16(.clk(clk), .ena(read_0_index[16]), .rst(1'b0), .d(x16), .q(out_data_0));
+register #(.N(32)) reg_rd0_17(.clk(clk), .ena(read_0_index[17]), .rst(1'b0), .d(x17), .q(out_data_0));
+register #(.N(32)) reg_rd0_18(.clk(clk), .ena(read_0_index[18]), .rst(1'b0), .d(x18), .q(out_data_0));
+register #(.N(32)) reg_rd0_19(.clk(clk), .ena(read_0_index[19]), .rst(1'b0), .d(x19), .q(out_data_0));
+register #(.N(32)) reg_rd0_20(.clk(clk), .ena(read_0_index[20]), .rst(1'b0), .d(x20), .q(out_data_0));
+register #(.N(32)) reg_rd0_21(.clk(clk), .ena(read_0_index[21]), .rst(1'b0), .d(x21), .q(out_data_0));
+register #(.N(32)) reg_rd0_22(.clk(clk), .ena(read_0_index[22]), .rst(1'b0), .d(x22), .q(out_data_0));
+register #(.N(32)) reg_rd0_23(.clk(clk), .ena(read_0_index[23]), .rst(1'b0), .d(x23), .q(out_data_0));
+register #(.N(32)) reg_rd0_24(.clk(clk), .ena(read_0_index[24]), .rst(1'b0), .d(x24), .q(out_data_0));
+register #(.N(32)) reg_rd0_25(.clk(clk), .ena(read_0_index[25]), .rst(1'b0), .d(x25), .q(out_data_0));
+register #(.N(32)) reg_rd0_26(.clk(clk), .ena(read_0_index[26]), .rst(1'b0), .d(x26), .q(out_data_0));
+register #(.N(32)) reg_rd0_27(.clk(clk), .ena(read_0_index[27]), .rst(1'b0), .d(x27), .q(out_data_0));
+register #(.N(32)) reg_rd0_28(.clk(clk), .ena(read_0_index[28]), .rst(1'b0), .d(x28), .q(out_data_0));
+register #(.N(32)) reg_rd0_29(.clk(clk), .ena(read_0_index[29]), .rst(1'b0), .d(x29), .q(out_data_0));
+register #(.N(32)) reg_rd0_30(.clk(clk), .ena(read_0_index[30]), .rst(1'b0), .d(x30), .q(out_data_0));
+
+always_comb rd_data0 = out_data_0;
+
+register #(.N(32)) reg_rd1_0(.clk(clk), .ena(read_1_index[0]), .rst(1'b0), .d(x00), .q(out_data_1));
+register #(.N(32)) reg_rd1_1(.clk(clk), .ena(read_1_index[1]), .rst(1'b0), .d(x01), .q(out_data_1));
+register #(.N(32)) reg_rd1_2(.clk(clk), .ena(read_1_index[2]), .rst(1'b0), .d(x02), .q(out_data_1));
+register #(.N(32)) reg_rd1_3(.clk(clk), .ena(read_1_index[3]), .rst(1'b0), .d(x03), .q(out_data_1));
+register #(.N(32)) reg_rd1_4(.clk(clk), .ena(read_1_index[4]), .rst(1'b0), .d(x04), .q(out_data_1));
+register #(.N(32)) reg_rd1_5(.clk(clk), .ena(read_1_index[5]), .rst(1'b0), .d(x05), .q(out_data_1));
+register #(.N(32)) reg_rd1_6(.clk(clk), .ena(read_1_index[6]), .rst(1'b0), .d(x06), .q(out_data_1));
+register #(.N(32)) reg_rd1_7(.clk(clk), .ena(read_1_index[7]), .rst(1'b0), .d(x07), .q(out_data_1));
+register #(.N(32)) reg_rd1_8(.clk(clk), .ena(read_1_index[8]), .rst(1'b0), .d(x08), .q(out_data_1));
+register #(.N(32)) reg_rd1_9(.clk(clk), .ena(read_1_index[9]), .rst(1'b0), .d(x09), .q(out_data_1));
+register #(.N(32)) reg_rd1_10(.clk(clk), .ena(read_1_index[10]), .rst(1'b0), .d(x10), .q(out_data_1));
+register #(.N(32)) reg_rd1_11(.clk(clk), .ena(read_1_index[11]), .rst(1'b0), .d(x11), .q(out_data_1));
+register #(.N(32)) reg_rd1_12(.clk(clk), .ena(read_1_index[12]), .rst(1'b0), .d(x12), .q(out_data_1));
+register #(.N(32)) reg_rd1_13(.clk(clk), .ena(read_1_index[13]), .rst(1'b0), .d(x13), .q(out_data_1));
+register #(.N(32)) reg_rd1_14(.clk(clk), .ena(read_1_index[14]), .rst(1'b0), .d(x14), .q(out_data_1));
+register #(.N(32)) reg_rd1_15(.clk(clk), .ena(read_1_index[15]), .rst(1'b0), .d(x15), .q(out_data_1));
+register #(.N(32)) reg_rd1_16(.clk(clk), .ena(read_1_index[16]), .rst(1'b0), .d(x16), .q(out_data_1));
+register #(.N(32)) reg_rd1_17(.clk(clk), .ena(read_1_index[17]), .rst(1'b0), .d(x17), .q(out_data_1));
+register #(.N(32)) reg_rd1_18(.clk(clk), .ena(read_1_index[18]), .rst(1'b0), .d(x18), .q(out_data_1));
+register #(.N(32)) reg_rd1_19(.clk(clk), .ena(read_1_index[19]), .rst(1'b0), .d(x19), .q(out_data_1));
+register #(.N(32)) reg_rd1_20(.clk(clk), .ena(read_1_index[20]), .rst(1'b0), .d(x20), .q(out_data_1));
+register #(.N(32)) reg_rd1_21(.clk(clk), .ena(read_1_index[21]), .rst(1'b0), .d(x21), .q(out_data_1));
+register #(.N(32)) reg_rd1_22(.clk(clk), .ena(read_1_index[22]), .rst(1'b0), .d(x22), .q(out_data_1));
+register #(.N(32)) reg_rd1_23(.clk(clk), .ena(read_1_index[23]), .rst(1'b0), .d(x23), .q(out_data_1));
+register #(.N(32)) reg_rd1_24(.clk(clk), .ena(read_1_index[24]), .rst(1'b0), .d(x24), .q(out_data_1));
+register #(.N(32)) reg_rd1_25(.clk(clk), .ena(read_1_index[25]), .rst(1'b0), .d(x25), .q(out_data_1));
+register #(.N(32)) reg_rd1_26(.clk(clk), .ena(read_1_index[26]), .rst(1'b0), .d(x26), .q(out_data_1));
+register #(.N(32)) reg_rd1_27(.clk(clk), .ena(read_1_index[27]), .rst(1'b0), .d(x27), .q(out_data_1));
+register #(.N(32)) reg_rd1_28(.clk(clk), .ena(read_1_index[28]), .rst(1'b0), .d(x28), .q(out_data_1));
+register #(.N(32)) reg_rd1_29(.clk(clk), .ena(read_1_index[29]), .rst(1'b0), .d(x29), .q(out_data_1));
+register #(.N(32)) reg_rd1_30(.clk(clk), .ena(read_1_index[30]), .rst(1'b0), .d(x30), .q(out_data_1));
+always_comb rd_data1 = out_data_1;
 
 
 endmodule
